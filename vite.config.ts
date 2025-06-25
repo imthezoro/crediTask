222c15dec40f257acd1a326998d0e740bb133116
Kap_God <kavinprasath2000@gmail.com>
2025-06-26 00:47:47 +0530

    Run on 8080 port

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8080,
+    host: '127.0.0.1', // Optional: force IPv4
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
